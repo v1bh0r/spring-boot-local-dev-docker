1584e6dd02c84d25a53e34cfc5a00c306fba9cd0
Vibhor Mahajan <vibhor@trantorinc.com>
2021-09-23 18:06:29 +0530

    Step 2 Added an api response

---
+++ src/main/java/com/trantorinc/springbootlocaldevdocker/APIController.java
@@ -0,0 +1,15 @@
+package com.trantorinc.springbootlocaldevdocker;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class APIController {
+    @GetMapping("/")
+    private ResponseEntity<String> getRoot() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
