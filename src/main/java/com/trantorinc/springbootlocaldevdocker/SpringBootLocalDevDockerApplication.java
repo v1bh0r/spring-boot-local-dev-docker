9b58505e213be4d8def01d2f1c30cdec712a6970
Vibhor Mahajan <vibhor@trantorinc.com>
2021-09-23 15:59:51 +0530

    Init

---
+++ src/main/java/com/trantorinc/springbootlocaldevdocker/SpringBootLocalDevDockerApplication.java
@@ -0,0 +1,13 @@
+package com.trantorinc.springbootlocaldevdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLocalDevDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLocalDevDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trantorinc/springbootlocaldevdocker/SpringBootLocalDevDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trantorinc.springbootlocaldevdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLocalDevDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
