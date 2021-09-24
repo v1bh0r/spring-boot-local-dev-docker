eaa717efa7f9bebdbd3598827937366086531819
Vibhor Mahajan <vibhor@trantorinc.com>
2021-09-24 17:02:46 +0530

    Enabled remote debugging

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dos2unix mvnw
-./mvnw spring-boot:run &
+./mvnw spring-boot:run -Drun.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005" &
 while true; do
   inotifywait -e modify,create,delete,move -r ./src/ && ./mvnw install
 done
