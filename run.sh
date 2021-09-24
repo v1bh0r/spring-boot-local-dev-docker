1b082bcd584b76f4bfb30c5ee3b4c27b777f1310
Vibhor Mahajan <vibhor@trantorinc.com>
2021-09-24 17:32:47 +0530

    Optimized by running maven compile instead of install

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 dos2unix mvnw
 ./mvnw spring-boot:run -Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005" &
 while true; do
-  inotifywait -e modify,create,delete,move -r ./src/ && ./mvnw install
+  inotifywait -e modify,create,delete,move -r ./src/ && ./mvnw compile
 done
